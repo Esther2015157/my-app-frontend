70ba373910ef8b5b50ebd96d80f4ade3716c3b0d
Esther2015157 <esthersly8@gmail.com>
2022-09-14 04:59:26 +0300

    added restaurant routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route,Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
 import NavBar from "./components/Navbar";
 import Home from "./components/Home";
 import Feedback from "./components/Feedback"
@@ -12,13 +12,13 @@ import About from "./components/About";
       <div className="App">
            <Router>
       <NavBar />
-      <Switch>
+      <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Feedback' element={<Feedback />} />
         <Route path='/review' element={<Reviews/>}/>
         <Route path='/About' element={<About/>}/> 
         <Route path= '/Restuarants' element={<Restaurants/>}/>
-        </Switch>
+        </Routes>
       </Router>
 
   </div>
