5947c5e051c7ee6db7a61dc14f9db8ab88ff9bb5
Esther2015157 <esthersly8@gmail.com>
2022-09-14 04:33:42 +0300

    Added route,router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route,Switch} from "react-router-dom";
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
+import Feedback from "./components/Feedback"
+ import Reviews from "./components/Reviews";
+ import Restaurants from "./components/Restaurants"
+import About from "./components/About";
+ function App() {
+    return (
+      <div className="App">
+           <Router>
+      <NavBar />
+      <Switch>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Feedback' element={<Feedback />} />
+        <Route path='/review' element={<Reviews/>}/>
+        <Route path='/About' element={<About/>}/> 
+        <Route path= '/Restuarants' element={<Restaurants/>}/>
+        </Switch>
+      </Router>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  </div>
   );
 }
 
